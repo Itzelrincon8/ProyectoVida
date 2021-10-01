c29a6e219af3684f27b70cf90c4563c7c99fcaa4
ItzelRincon8 <itzelrdll8@gmail.com>
2021-10-01 01:04:11 -0500

    add services #6

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
 import { DataService } from '../data.service';
-
+//this is a issue comment
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
